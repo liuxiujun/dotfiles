96d803a775b89541c53627d84ecd45540a4e8119
liuxiujun <liuxiujun09@outlook.com>
2025-06-17 17:32:50 +0800

    +lsp

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -26,10 +26,6 @@ vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- For nvim-tree.lua
--- default leader key: \
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
-
 -- custom
 vim.keymap.set("n", "<leader>s", ":w<CR>", opts)
 vim.keymap.set("n", "<leader>x", ":q<CR>", opts)
