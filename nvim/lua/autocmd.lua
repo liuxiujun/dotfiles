f9990dc701b1b3a12bfc6d1e206852ff9fb8c7ae
liuxiujun <liuxiujun09@outlook.com>
2025-07-07 01:17:09 +0800

    +win32yank

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -14,3 +14,19 @@
 --         vim.bo.filetype = "perl"
 --     end,
 -- })
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
+
