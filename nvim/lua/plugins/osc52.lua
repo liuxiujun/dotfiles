69394a72817b360179acf9f4016cdb8f5e66b4d0
liuxiujun <liuxiujun09@outlook.com>
2025-07-11 12:18:01 +0000

    modified osc52

--- nvim/lua/plugins/osc52.lua
+++ nvim/lua/plugins/osc52.lua
@@ -1,3 +1,24 @@
+-- return {
+--     "ojroques/nvim-osc52",
+--     event = "VeryLazy",
+--     config = function()
+--         require("osc52").setup({
+--             max_length = 0,
+--             trim = false,
+--             silent = false,
+--         })
+--
+--         -- 自动复制到本地剪贴板
+--         vim.api.nvim_create_autocmd("TextYankPost", {
+--             callback = function()
+--                 if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+--                     require("osc52").copy_register("+")
+--                 end
+--             end,
+--         })
+--     end,
+-- }
+-- lazy.nvim 示例
 return {
     "ojroques/nvim-osc52",
     event = "VeryLazy",
@@ -8,11 +29,11 @@ return {
             silent = false,
         })
 
-        -- 自动复制到本地剪贴板
+        -- 当使用 `"+y` 或默认 `y` 时自动触发复制
         vim.api.nvim_create_autocmd("TextYankPost", {
             callback = function()
-                if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
-                    require("osc52").copy_register("+")
+                if vim.v.event.operator == "y" then
+                    require("osc52").copy_register(vim.v.event.regname == "" and '"' or vim.v.event.regname)
                 end
             end,
         })
