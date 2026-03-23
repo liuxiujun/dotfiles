8a608db8c16912bebac55fe341fd67c529b1f476
liuxiujun <liuxiujun09@outlook.com>
2026-03-23 15:48:34 +0800

    options.lua中设置nvim需要自动识别多种编码

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,6 +7,10 @@ vim.g.mapleader = ","
 -- Hint: use `:h <option>` to figure out the meaning if needed
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
 
+-- Set Encoding
+vim.opt.encoding = "utf-8"
+vim.opt.fileencodings = "utf-8,gbk,gb18030,latin1,ucs-bom"
+
 local system = require("config.system");
 
 if system.is_windows or system.is_wsl then
