6ebb6a8537faf4ac1f831c49952d53b3ebb81942
liuxiujun <liuxiujun09@outlook.com>
2026-04-01 14:47:56 +0800

    change CRLF to LF

--- nvim/lua/plugins/osc52.lua
+++ nvim/lua/plugins/osc52.lua
@@ -1,24 +1,3 @@
--- return {
---     "ojroques/nvim-osc52",
---     event = "VeryLazy",
---     config = function()
---         require("osc52").setup({
---             max_length = 0,
---             trim = false,
---             silent = false,
---         })
---
---         -- 自动复制到本地剪贴板
---         vim.api.nvim_create_autocmd("TextYankPost", {
---             callback = function()
---                 if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
---                     require("osc52").copy_register("+")
---                 end
---             end,
---         })
---     end,
--- }
--- lazy.nvim 示例
 return {
     "ojroques/nvim-osc52",
     event = "VeryLazy",
