1828fc38368662b24a36b2502533143c0ad11af7
liuxiujun <liuxiujun09@outlook.com>
2026-05-25 16:57:21 +0800

    修改lsp:php -> phpactor

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -191,11 +191,11 @@ vim.lsp.config("gopls", {
     },
 })
 
-vim.lsp.config("php", {
+vim.lsp.config("phpactor", {
     cmd = { 'phpactor', 'language-server' },
     filetypes = { 'php' },
     root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
-    workspace_required = true,
+    workspace_required = false,
     init_options = {
         ["language_server_phpstan.enabled"] = false,
         ["language_server_psalm.enabled"] = false,
@@ -275,5 +275,5 @@ vim.lsp.enable({
     "ts_ls",
     "clangd",
     "jdtls",
-    "php",
+    "phpactor",
 })
